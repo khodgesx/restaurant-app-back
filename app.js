073b9ba84fcd04393341ba8150bb64a9041eac00
khodges <zkellyhodges@gmail.com>
2022-04-05 12:52:07 -0700

    changed url encoded to false

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use(cors())
 // }
 // app.use(cors(corsOptions))
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 app.use(session({
     secret: process.env.SESSION_SECRET,
